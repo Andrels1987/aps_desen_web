bbd10bb2b44398e55ad6cdaf9df34ddebe43750c
Andrels <alspersonalmail@gmail.com>
2023-05-26 15:54:14 -0300

    atualização 5

--- pages/context/GameContext.js
+++ pages/context/GameContext.js
@@ -1,5 +1,6 @@
 // 'use client'
 import { createContext, useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
 import {
     getDatabase,
     ref,
@@ -16,6 +17,30 @@ import {
     {Qid:3,question:'what is angular js',rightAnswer: 'is a structural framework for dynamic web apps.', answers: [{id:1,answer:'Termo usado para descrever um Website'},{id:2, answer: 'Semelhante ao java, só que mais leve'},{id:3, answer:' is a structural framework for dynamic web apps.'}]},
     {Qid:4,question:'what is solid js',rightAnswer: 'is a JavaScript framework that supports binding data to elements that it then syncs and displays on web pages', answers: [{id:1,answer:'Codigo para compilar javascript'},{id:2, answer: 'is a JavaScript framework that supports binding data to elements that it then syncs and displays on web pages'},{id:3, answer:'Framework usado para criar REST APIs'}]},
 ] */
+
+
+import { getAuth,
+     createUserWithEmailAndPassword,
+     signInWithEmailAndPassword,
+     sendEmailVerification,
+     sendPasswordResetEmail,
+     signOut} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+const database = getDatabase(app);
 
 
 export const GameContext = createContext();
